5acabf0507024a8e8e6464060ee9a6efb42b7509
Gabriel Dodowei <gabrieldodowei@gmail.com>
2026-09-23 17:53:02 +0100

    Turned the button into a global component

---
+++ src/components/RequestAProposalButton.tsx
@@ -0,0 +1,9 @@
+import { Dispatch, SetStateAction } from "react";
+
+export default function RequestAProposalButton({ setIsOpen }: { setIsOpen: Dispatch<SetStateAction<boolean>> }) {
+    return (
+        <button type="button" onClick={() => setIsOpen(true)}
+            className="bg-accent hover:bg-accent-hover cursor-pointer px-3 py-2 rounded-full text-white font-medium"
+        >Request a Proposal</button>
+    )
+}
